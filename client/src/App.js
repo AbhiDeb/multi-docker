35a64922dc2928f5e4ba9a378ea00190999d7959
Abhitesh Debnath <abhideb@Abhiteshs-MacBook-Air.local>
2021-05-28 00:11:37 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/" center="true">Home</Link>
+          &nbsp;&nbsp;
+          <Link to="/otherpage" center="true">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
